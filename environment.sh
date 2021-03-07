a7d481897ad6ad79139dad77717a72234f1de0d0
cihuang123 <cihuang@nctu.edu.tw>
2021-03-07 16:47:56 +0800

    CMake.list

--- environment.sh
+++ environment.sh
@@ -1,1 +1,1 @@
-source /opt/ros/melodic/setup.bash && source /content/2021-lab1/catkin_ws/devel/setup.bash
+source /opt/ros/melodic/setup.bash && source /content/2021-lab2/catkin_ws/devel/setup.bash
